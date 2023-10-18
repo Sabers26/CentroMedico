3ec871eaabd86ad4ed37d8cf25935d6849b14394
erwinnunez <er.nunez@duocuc.cl>
2023-10-17 21:05:44 -0300

    js

--- app/static/colaborador.js
+++ app/static/colaborador.js
@@ -17,3 +17,33 @@ select.addEventListener('change', function() {
     elemento2.style.display = 'none'; // Muestra el elemento
   }
 });
+
+function validar() {
+
+  var rutInput = document.getElementById("rut");
+  var rut = rutInput.value;
+
+  // Expresión regular que cumple con los requisitos
+  var patron = /^[0-9]+[-|‐]{1}[0-9kK]{1}$/;
+
+  // Comprobamos si el RUT cumple con el patrón y la longitud
+  if (!patron.test(rut) || rut.length > 10) {
+    rutInput.value = ""; // Borra el campo si es inválido
+    alert("rut no válido");
+    
+  }
+
+  var nombreApellidoInput = document.getElementById("nombreApellido");
+  var nombreApellido = nombreApellidoInput.value;
+
+  // Expresión regular que cumple con los requisitos
+  var patron = /^[A-Za-z ]{1,30}$/;
+
+  // Comprobamos si el nombre y apellido NO cumplen con el patrón
+  if (!patron.test(nombreApellido)) {
+    alert("Nombre y apellido no válidos");
+    nombreApellidoInput.value = ""; // Borra el campo si es inválido
+  }
+
+};
+
